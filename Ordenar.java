6eca9d8aa73ffcccd8f33f2147c49addb6603ceb
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2023-10-03 16:54:06 +0200

    Add files via upload

---
+++ Ordenar.java
@@ -0,0 +1,50 @@
+/*
+
+1.º DAM - Programación Ejercicios
+Ejercicios Tema 3. Estructuras condicionales
+3. Ordenar los 2 numeros solicitados de menor a mayor
+*/
+import java.util.Scanner;
+
+public class Ordenar {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+		int num1;
+		int num2;
+
+      System.out.println("Dime el primer numero ");
+        num1 = Integer.parseInt(lector.nextLine());
+              System.out.println("Dime el segundo numero ");
+        num2 = Integer.parseInt(lector.nextLine());
+        
+
+        lector.close(); // cierro scanner
+  
+
+       
+        if (num1 == num2) {
+			           System.out.println("El orden  de menor a mayor es el siguiente: ");
+
+           System.out.println(num1);
+		   System.out.println(num2);
+
+        } else if (num1 < num2) {
+			           System.out.println("El orden  de menor a mayor es el siguiente: ");
+
+           System.out.println(num1);
+		   System.out.println(num2);
+        } else {
+			           System.out.println("El orden  de menor a mayor es el siguiente: ");
+
+            
+		   System.out.println(num2);
+		     System.out.println(num1);
+        }
+		
+		
+
+
+      
+    }
+}
+
