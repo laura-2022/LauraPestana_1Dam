f9b66154db90ee5c88e2e1dab0c376ddb35be8e2
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2023-10-03 16:35:29 +0200

    Add files via upload

---
+++ MenorMayor.java
@@ -0,0 +1,33 @@
+1.º DAM - Programación Ejercicios
+
+2. Escribe un programa que solicite al usuario dos números e indique si el primer número introducido
+es mayor, menor o igual que el segundo
+
+*/
+import java.util.Scanner;
+
+public class Impares {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+        System.out.println("Dime primer numero ");
+        int num1 = Integer.parseInt(lector.nextLine());
+        System.out.println("Dame segundo numero");
+        int num2 = Integer.parseInt(lector.nextLine());
+
+        String op;
+
+        if (num1 == num2) {
+            op = "igual";
+        } else if (num1 < num2) {
+            op = "menor";
+        } else {
+            op = "mayor";
+        }
+
+        lector.close(); // cierro scanner
+
+        System.out.println("el primer numero " + num1 + " es " + op + " al segundo numero ingresado");
+    }
+}
+
