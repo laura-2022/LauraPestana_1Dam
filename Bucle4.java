5b42e9d0fd527a1811e765267c88e858360ab2a3
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2023-10-06 17:03:32 +0200

    Add files via upload

---
+++ Bucle4.java
@@ -0,0 +1,28 @@
+//programa que calcule el producto de 2 numeros y que los convierta en suma . Por ejemplo 2x3=3+3;//2 veces 3
+
+
+
+
+public class Bucle4{
+    public static void main(String[] args) {
+	
+		int cont=0;
+
+		System.out.println("-------------numeros impares del 1 al 100 indicando cuantos hay en total--------------------");
+	 
+		
+	  
+	 
+		for(int i=1;1000>=i;i++){
+			if(i%2!=0){
+				cont++;
+				
+				System.out.println(i);
+			}
+			
+		}
+
+		System.out.println("contador:="+cont);
+    }
+}
+
