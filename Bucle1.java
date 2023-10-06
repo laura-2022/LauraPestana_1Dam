ca1873826e86e1c67d5a1267f32be621a5fb366f
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2023-10-06 15:56:38 +0200

    Add files via upload

---
+++ Bucle1.java
@@ -0,0 +1,29 @@
+
+/*
+1.º DAM - Programación Ejercicios
+Ejercicios Tema 3. Estructuras condicionales
+
+*/
+
+public class Bucle1{
+    public static void main(String[] args) {
+		
+//bucle for que imprima numero entre el 45 a 100
+	
+	
+
+       System.out.println("-------------numeros desde el 45 al 100--------------------");
+      for(int i=45;i<101;i++){
+		  System.out.print(i +"-");
+	  }
+	   
+			   
+		 
+		
+		
+		
+
+
+    }
+}
+
