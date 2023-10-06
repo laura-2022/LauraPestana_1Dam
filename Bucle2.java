555155fedc7cb2b078adfb466eac6dc43bcd3dbd
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2023-10-06 16:10:48 +0200

    Add files via upload

---
+++ Bucle2.java
@@ -0,0 +1,25 @@
+
+
+public class Bucle2{
+    public static void main(String[] args) {
+		
+//bucle for que imprima numero esuma entre 1 a 1000
+	
+
+       System.out.println("-------------suma de numeros entre 1 a 1000--------------------");
+	int suma=0;
+      for(int i=0;i<=1000;i++){
+		  suma=suma+i;
+	  }
+	   
+			       System.out.println("-el total de la suma de numeros es : "+ suma);
+   
+		 
+		
+		
+		
+
+
+    }
+}
+
