cd6c4d66e5792c134f4eb1055b537f755ebd38b4
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2023-10-10 14:55:54 +0200

    Add files via upload

---
+++ Bucle7.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Bucle7{
+    public static void main(String[] args) {
+		Scanner lector=new Scanner (System.in);
+		
+        double a ; // el primer numero
+        int b;    // la potencia aplicada en negativo
+		double resultado=0; // declaramos resultado
+
+
+  System.out.println("Dime numero a (decimal)");
+       a=Double.parseDouble(lector.nextLine());//leo como double
+	   
+       System.out.println("Dime numero b (entero negativo)");
+       b=Integer.parseInt(lector.nextLine());// leo como entero
+		
+
+for (int i = 0; i < b; i++) {
+             resultado =resultado* a; // Multiplicar 'a' por sí mismo 'b' veces
+			         System.out.println(a + " elevado a la potencia " + b + " es igual a " + resultado);
+
+        }
+
+        System.out.println(a + " elevado a la potencia " + b + " es igual a " + resultado);
+
+        // Cerramos el scanner para liberar recursos.
+        lector.close();
+    }
+}
+
+     
