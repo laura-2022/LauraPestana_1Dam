bf035570f577a38240aea8ff4cf55b6e9840c389
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2023-10-24 20:26:07 +0200

    Add files via upload

--- Metodos2.java
+++ Metodos2.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
+
 public class Metodos2 {
+
+    // Códigos ANSI para colores
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+	public static final String BGAZUL="\u001B[44m";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,9 +15,14 @@ public class Metodos2 {
 
         double area = calcularAreaCircunferencia(radio);
         double longitud = calcularLongitudCircunferencia(radio);
+        
+		System.out.println(BGAZUL+"**CIRCUNFERENCIA **"+RESET);
 
-        System.out.println("El área de la circunferencia es: " + area);
-        System.out.println("La longitud de la circunferencia es: " + longitud);
+        System.out.print("El área de la circunferencia es: ");
+        System.out.println(GREEN + String.format("%.2f cm2", area) + RESET);
+
+        System.out.print("La longitud de la circunferencia es: ");
+        System.out.println(RED + String.format("%.2f cm", longitud) + RESET);
     }
 
     // Método para calcular el área de la circunferencia
