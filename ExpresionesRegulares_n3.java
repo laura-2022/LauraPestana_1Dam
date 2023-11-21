89d3f7e932dc0a2551d8cf1324e25cbcf613de44
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2023-11-21 15:45:07 +0100

    Update ExpresionesRegulares_n3.java

--- ExpresionesRegulares_n3.java
+++ ExpresionesRegulares_n3.java
@@ -9,28 +9,21 @@ public class ExpresionesRegulares_n3 {
 		        System.out.print("Ingresa una frase: ");
 		        String frase3 = scanner.nextLine();
 		        System.out.println("Número de palabras: " + contarPalabras(frase3));
-
-		        
-
-
-		
-		
-
-		  
-
 		}
 
-		    private static int contarPalabras(String frase) {
+		private static int contarPalabras(String frase) {
 		        int contador = 0;
 		        boolean palabra = false;
 
 		        for (int i = 0; i < frase.length(); i++) {
 		            if (frase.charAt(i) == ' ' || frase.charAt(i) == '\n' || frase.charAt(i) == '\t') {
 		                palabra = false;
-		            } else if (palabra) {
+		            } else if (!palabra) {
+		                palabra = true;
 		                contador++;
 		            }
 		        }
-
+				return contador;
+		    }
 		
 }
